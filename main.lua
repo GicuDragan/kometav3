9c374bc12fff07d0058d3881a59c8735272edf34
GicuDragan <115421986+GicuDragan@users.noreply.github.com>
2023-03-27 16:23:37 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,43 +1,122 @@
-getgenv().SecureMode = true
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+local lplr = game.Players.LocalPlayer
+local camera = game:GetService("Workspace").CurrentCamera
+local CurrentCamera = workspace.CurrentCamera
+local worldToViewportPoint = CurrentCamera.worldToViewportPoint
 
-local Window = Rayfield:CreateWindow({
-    Name = "Kometa v3",
-    LoadingTitle = "Loading...",
-    LoadingSubtitle = "by Luci",
-    ConfigurationSaving = {
-       Enabled = true,
-       FolderName = nil, -- Create a custom folder for your hub/game
-       FileName = "Hub"
-    },
-    Discord = {
-       Enabled = false,
-       Invite = "Kometa", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD.
-       RememberJoins = false -- Set this to false to make them join the discord every time they load it up
-    },
-    KeySystem = true, -- Set this to true to use our key system
-    KeySettings = {
-       Title = "Kometa v3 Key System",
-       Subtitle = "",
-       Note = "Join the discord for free premium account! (discord.gg/MvewhvQNh8)",
-       FileName = "HubKey",
-       SaveKey = true,
-       GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-       Key = "94gxpu39-if93ayuk-s1q5exko-ud84qgo3-g8a4xdmb"
-    }
- })
- 
- Rayfield:Notify({
-    Title = "Welcome",
-    Content = "Thank you for executing the script ⭐",
-    Duration = 10,
-    Image = 4483362458,
-    Actions = { -- Notification Buttons
-       Ignore = {
-          Name = "Okay!",
-          Callback = function()
-          print("The user tapped Okay!")
-       end
-    },
- },
- })
+local HeadOff = Vector3.new(0, 0.5, 0)
+local LegOff = Vector3.new(0,3,0)
+
+for i,v in pairs(game.Players:GetChildren()) do
+local BoxOutline = Drawing.new("Square")
+BoxOutline.Visible = false
+BoxOutline.Color = Color3.new(0,0,0)
+BoxOutline.Thickness = 3
+BoxOutline.Transparency = 1
+BoxOutline.Filled = false
+
+local Box = Drawing.new("Square")
+Box.Visible = false
+Box.Color = Color3.new(1,1,1)
+Box.Thickness = 1
+Box.Transparency = 1
+Box.Filled = false
+
+function boxesp()
+    game:GetService("RunService").RenderStepped:Connect(function()
+        if v.Character ~= nil and v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("HumanoidRootPart") ~= nil and v ~= lplr and v.Character.Humanoid.Health > 0 then
+            local Vector, onScreen = camera:worldToViewportPoint(v.Character.HumanoidRootPart.Position)
+
+            local RootPart = v.Character.HumanoidRootPart
+            local Head = v.Character.Head
+            local RootPosition, RootVis = worldToViewportPoint(CurrentCamera, RootPart.Position)
+            local HeadPosition = worldToViewportPoint(CurrentCamera, Head.Position + HeadOff)
+            local LegPosition = worldToViewportPoint(CurrentCamera, RootPart.Position - LegOff)
+
+            if onScreen then
+                BoxOutline.Size = Vector2.new(1000 / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
+                BoxOutline.Position = Vector2.new(RootPosition.X - BoxOutline.Size.X / 2, RootPosition.Y - BoxOutline.Size.Y / 2)
+                BoxOutline.Visible = true
+
+                Box.Size = Vector2.new(1000 / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
+                Box.Position = Vector2.new(RootPosition.X - Box.Size.X / 2, RootPosition.Y - Box.Size.Y / 2)
+                Box.Visible = true
+
+                if v.TeamColor == lplr.TeamColor then
+                    BoxOutline.Visible = false
+                    Box.Visible = false
+                else
+                    BoxOutline.Visible = true
+                    Box.Visible = true
+                end
+
+            else
+                BoxOutline.Visible = false
+                Box.Visible = false
+            end
+        else
+            BoxOutline.Visible = false
+            Box.Visible = false
+        end
+    end)
+end
+coroutine.wrap(boxesp)()
+end
+
+game.Players.PlayerAdded:Connect(function(v)
+local BoxOutline = Drawing.new("Square")
+BoxOutline.Visible = false
+BoxOutline.Color = Color3.new(0,0,0)
+BoxOutline.Thickness = 3
+BoxOutline.Transparency = 1
+BoxOutline.Filled = false
+
+local Box = Drawing.new("Square")
+Box.Visible = false
+Box.Color = Color3.new(1,1,1)
+Box.Thickness = 1
+Box.Transparency = 1
+Box.Filled = false
+
+function boxesp()
+    game:GetService("RunService").RenderStepped:Connect(function()
+        if v.Character ~= nil and v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("HumanoidRootPart") ~= nil and v ~= lplr and v.Character.Humanoid.Health > 0 then
+            local Vector, onScreen = camera:worldToViewportPoint(v.Character.HumanoidRootPart.Position)
+
+            local RootPart = v.Character.HumanoidRootPart
+            local Head = v.Character.Head
+            local RootPosition, RootVis = worldToViewportPoint(CurrentCamera, RootPart.Position)
+            local HeadPosition = worldToViewportPoint(CurrentCamera, Head.Position + HeadOff)
+            local LegPosition = worldToViewportPoint(CurrentCamera, RootPart.Position - LegOff)
+
+            if onScreen then
+                BoxOutline.Size = Vector2.new(1000 / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
+                BoxOutline.Position = Vector2.new(RootPosition.X - BoxOutline.Size.X / 2, RootPosition.Y - BoxOutline.Size.Y / 2)
+                BoxOutline.Visible = true
+
+                Box.Size = Vector2.new(1000 / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
+                Box.Position = Vector2.new(RootPosition.X - Box.Size.X / 2, RootPosition.Y - Box.Size.Y / 2)
+                Box.Visible = true
+
+                if v.TeamColor == lplr.TeamColor then
+                    BoxOutline.Visible = false
+                    Box.Visible = false
+                else
+                    BoxOutline.Visible = true
+                    Box.Visible = true
+                end
+
+            else
+                BoxOutline.Visible = false
+                Box.Visible = false
+            end
+        else
+            BoxOutline.Visible = false
+            Box.Visible = false
+        end
+    end)
+end
+coroutine.wrap(boxesp)()
+end)
+
+---Created By 0x83
+end)
